7b5080f502e44dc9eab9f28d0ac49e8be649f846
Ubuntu <ubuntu@ip-10-0-1-133.ec2.internal>
2025-07-20 15:43:12 +0000

    Add Dockerfile to Dockerize Node.js app

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/*', (req, res) => {
-  res.send('Hello This is Sagar -- Congrats Node app is working ');
+  res.send('Hello  Sagar -- Congrats Node app is working ');
 });
 
 app.listen(PORT, HOST);
