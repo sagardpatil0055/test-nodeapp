66d2319d2f7712c43f8e02062250ae0e27b8df4b
sagardpatil0055 <sagardpatil0055@gmail.com>
2025-07-21 03:54:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/*', (req, res) => {
-  res.send('Great Sagar -- Congrats Node app is working ');
+  res.send('Great Sagar -- Congrats Node app is working \n You are really great');
 });
 
 app.listen(PORT, HOST);
